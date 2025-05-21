a181931c4870862aa75f3df104d3052b91e68c15
Swaraj Sahani <swarajsahani8930@gmail.com>
2025-05-21 17:53:35 +0530

    added audio function

--- say.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-def say(text):
-    engine = pyttsx3.init()
-    print("say the ---> ", text)
-    engine.say(text)
-    engine.runAndWait()
-    print(f"says text: {text}")
-    return
-
